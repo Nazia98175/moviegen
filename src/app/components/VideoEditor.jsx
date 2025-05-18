60cd4b4d5263c851d0719115b4460155b4663464
rakesh olla <rakesh10448olla@gmail.com>
2025-05-18 22:58:41 +0530

    code merge

--- src/app/components/VideoEditor.jsx
+++ src/app/components/VideoEditor.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import SectionTag from "./common/SectionTag";
 import VideoEditorCards from "./VideoEditorCards";
+import Image from "next/image";
 
 const VideoEditor = () => {
   const heading = "common_heading !font-suisseintl !font-medium";
-  const para = "common_para max-w-[550px] lg:max-w-[700px] !font-geist";
+  const para = "common_para max-w-[560px] !font-geist";
   return (
-    <>
-      <div className="pt-20 md:pt-24 lg:pt-32 2xl:max-w-[1040px] 3xl:max-w-[1436px] mx-auto px-4 md:px-5 xl:px-0">
-        <SectionTag tagclass="hidden" title="AI Video Editor & Generator" />
-        <div className="text-center">
-          <div className={heading}>
-            <h1>Edit & Generate Videos</h1>
-            <h2 className="text-[#E1E7F5]  md:pt-4">Like a Pro With AI</h2>
-          </div>
-          <p className={para}>
-            Simple video editing for everyone. Add subtitles, text, music, and
-            much more to your videos with just a few clicks.
-          </p>
+    <div className="pt-20 md:pt-24 lg:pt-32 max-w-[1120px] mx-auto px-4 md:px-5 xl:px-0 relative">
+      <Image
+        className="w-full max-w-[952px] absolute left-1/2  translate-x-[-50%] top-[30%] z-[0]"
+        src={"/images/png/generate-video-blur.png"}
+        height={100}
+        width={400}
+        alt="hero-bg-blur"
+      />
+      <SectionTag tagclass="hidden" title="AI Video Editor & Generator" />
+      <div className="text-center">
+        <div className={heading}>
+          <h2>Edit & Generate Videos</h2>
+          <h2 className="text-[#E1E7F5]  md:pt-4">Like a Pro With AI</h2>
         </div>
-        <VideoEditorCards />
+        <p className={para}>
+          Simple video editing for everyone. Add subtitles, text, music, and
+          much more to your videos with just a few clicks.
+        </p>
       </div>
-    </>
+      <VideoEditorCards />
+    </div>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import BackToTop from "./components/common/BackToTop";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
@@ -11,8 +12,17 @@ import VideoEditorHero from "./components/VideoEditorHero";
 export default function Home() {
   return (
     <>
-      <Hero />
-      <VideoEditor />
+      <main className="relative">
+        <Image
+          className="w-full max-w-[952px] absolute left-1/2  translate-x-[-50%] top-[30%] z-[0]"
+          src={"/images/png/hero-bg-blur.png"}
+          height={100}
+          width={400}
+          alt="hero-bg-blur"
+        />
+        <Hero />
+        <VideoEditor />
+      </main>
       <VideoEditorHero />
       <Features />
       <Pricing />
